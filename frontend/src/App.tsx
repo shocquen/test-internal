a6299dbe8fa9e0ba27601ef87cc8a27736429107
Souria-Saky Hocquenghem <shocquen@protonmail.com>
2024-06-28 11:26:38 +0200

    feat: add chakra icons, will be usefull for the subject

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
+import { AddIcon, PhoneIcon, WarningIcon } from "@chakra-ui/icons";
 import { Button, HStack, Stack, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { useGetUsersQuery } from "./stores/api/users";
 
 function App() {
   const { data, isLoading, isError } = useGetUsersQuery();
   const [count, setCount] = useState(0);
-
   return (
     <>
-      <Stack height="100vh" bg="blue.100">
+      <Stack p={2} height="100vh" bg="blue.50">
         <HStack>
-          <Button onClick={() => setCount((count) => count + 1)}> + </Button>
+          <Button
+            colorScheme="blue"
+            size="lg"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            <AddIcon boxSize={6} />
+          </Button>
           <Text>{count}</Text>
         </HStack>
 
         {(isLoading || isError) && <Text>Loading...</Text>}
-        {data?.map((user) => (
-          <Text key={user._id}>{user._id}</Text>
-        ))}
+        {data?.map((user) => <Text key={user._id}>{user._id}</Text>)}
       </Stack>
     </>
   );
