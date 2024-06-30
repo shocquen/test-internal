c84e65a1ed2c1fa9843df3d92b300993f7110280
Souria-Saky Hocquenghem <shocquen@protonmail.com>
2024-06-30 23:32:58 +0200

    feat(form): use react hook for logics
    disable and enable confirm button, pass data by state over next page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,33 @@
 import { Button, Icon, Text, VStack } from "@chakra-ui/react";
 import { Buildings, User } from "@phosphor-icons/react";
 import { PrimaryLayout } from "../components/layouts";
+import { useState } from "react";
 
 export default function Index() {
+	const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+	const [whichButtonSelected, setButtonSelected] = useState("candidate");
+
+	const enableConfirmButton = function (
+		event: React.MouseEvent<HTMLButtonElement>
+	) {
+		setIsButtonDisabled(false);
+		setButtonSelected(event.currentTarget.name);
+	};
+
 	return (
 		<PrimaryLayout
 			heading="Qui êtes-vous ?"
 			text="Sélectionnez si vous êtes un agent ou bien si vous êtes un candidat."
 			buttonText="Connexion rapide"
+			isConfirmButtonDisabled={isButtonDisabled}
+			formData={{ selected: whichButtonSelected }}
 		>
 			<VStack width="full" gap="8px">
-				<Button variant="brandPrimary">
+				<Button
+					name="agent"
+					onClick={enableConfirmButton}
+					variant="brandPrimary"
+				>
 					<Icon
 						bg="gray.100"
 						as={Buildings}
@@ -21,7 +38,11 @@ export default function Index() {
 					<Text>Je suis un agent</Text>
 				</Button>
 
-				<Button variant="brandPrimary">
+				<Button
+					name="candidate"
+					onClick={enableConfirmButton}
+					variant="brandPrimary"
+				>
 					<Icon
 						bg="gray.100"
 						as={User}
