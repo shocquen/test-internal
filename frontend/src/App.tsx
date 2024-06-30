7e3c7a7a5502601f3d0dd20d5c4ffdeb966dde27
Souria-Saky Hocquenghem <shocquen@protonmail.com>
2024-06-30 18:38:09 +0200

    feat: confirmation page started and rooted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,29 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./pages/about";
-import Contact from "./pages/contact";
 import ErrorPage from "./pages/error";
 import Index from "./pages/index";
+import Confirmation from "./pages/confirmation";
+import OldIndex from "./pages/oldIndex";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Index />,
-    errorElement: <ErrorPage />,
-  },
+	{
+		path: "/",
+		element: <Index />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "/confirmation",
+		element: <Confirmation />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "/old",
+		element: <OldIndex />,
+		errorElement: <ErrorPage />,
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/pages/confirmation.tsx
@@ -0,0 +1,37 @@
+import { Button, Icon, Text, VStack } from "@chakra-ui/react";
+import { Buildings, User } from "@phosphor-icons/react";
+import { PrimaryLayout } from "../components/layouts";
+
+export default function Confirmation() {
+	return (
+		<PrimaryLayout
+			heading="Qui êtes-vous ?"
+			text="Sélectionnez si vous êtes un agent ou bien si vous êtes un candidat."
+			buttonText="Connexion rapide"
+		>
+			<VStack width="full" gap="8px">
+				<Button variant="brandPrimary">
+					<Icon
+						bg="gray.100"
+						as={Buildings}
+						boxSize="56px"
+						p="14px"
+						borderRadius="full"
+					/>
+					<Text>Je suis un agent</Text>
+				</Button>
+
+				<Button variant="brandPrimary">
+					<Icon
+						bg="gray.100"
+						as={User}
+						boxSize="56px"
+						p="14px"
+						borderRadius="full"
+					/>
+					<Text>Je suis un candidat</Text>
+				</Button>
+			</VStack>
+		</PrimaryLayout>
+	);
+}
