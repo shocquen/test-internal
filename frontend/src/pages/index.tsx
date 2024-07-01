dd724cd06835e97d08d518f25cc0710c55fa288b
Souria-Saky Hocquenghem <shocquen@protonmail.com>
2024-07-01 17:34:07 +0200

    refactor(Index): moved the func to run onClick action button here

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Button, Icon, Text, VStack } from "@chakra-ui/react";
 import { Buildings, User } from "@phosphor-icons/react";
 import { PrimaryLayout } from "../components/layouts";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Index() {
 	const [isButtonDisabled, setIsButtonDisabled] = useState(true);
@@ -10,17 +11,23 @@ export default function Index() {
 	const enableConfirmButton = function (
 		event: React.MouseEvent<HTMLButtonElement>
 	) {
+		event.preventDefault();
 		setIsButtonDisabled(false);
 		setButtonSelected(event.currentTarget.name);
 	};
+	const navigate = useNavigate();
 
+	const handleSubmit = () =>
+		navigate("/confirmation", {
+			state: { selected: whichButtonSelected },
+		});
 	return (
 		<PrimaryLayout
 			heading="Qui êtes-vous ?"
 			text="Sélectionnez si vous êtes un agent ou bien si vous êtes un candidat."
 			buttonText="Connexion rapide"
 			isConfirmButtonDisabled={isButtonDisabled}
-			formData={{ selected: whichButtonSelected }}
+			confirmButton={handleSubmit}
 		>
 			<VStack width="full" gap="8px">
 				<Button
