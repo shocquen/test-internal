13725707c673a38daf3302eed532d10d957ec22d
Souria-Saky Hocquenghem <shocquen@protonmail.com>
2024-07-02 14:38:43 +0200

    build(jwt): add lib and env var for jwt

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
 
 export interface IUser extends mongoose.Document {
-  first_name: string;
-  last_name: string;
+	first_name: string;
+	last_name: string;
+	email: string;
+	number: string;
+	createdAt: Date;
+	updatedAt: Date;
 }
 
 const userSchema: mongoose.Schema<IUser> = new mongoose.Schema(
-  {
-    first_name: {
-      type: String,
-      default: null,
-    },
-    last_name: {
-      type: String,
-      default: null,
-    },
-  },
-  { timestamps: true, versionKey: false }
+	{
+		first_name: {
+			type: String,
+			default: null,
+		},
+		last_name: {
+			type: String,
+			default: null,
+		},
+	},
+	{ timestamps: true, versionKey: false }
 );
 
 export const UserModel = mongoose.model<IUser>("User", userSchema);
