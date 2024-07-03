4bc5d1697f668cca2cef4cc16b31404a6178631e
Souria-Saky Hocquenghem <shocquen@protonmail.com>
2024-07-03 08:10:01 +0200

    feat(back-api): set headers for bearer token

--- frontend/src/stores/api/api.ts
+++ frontend/src/stores/api/api.ts
@@ -1,10 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const api = createApi({
-  reducerPath: "api" as const,
-  // TIPS: add headers here
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000", mode: "cors" }),
-  endpoints: () => ({}),
+	reducerPath: "api" as const,
+	baseQuery: fetchBaseQuery({
+		baseUrl: "http://localhost:3000",
+		mode: "cors",
+		prepareHeaders: (headers) => {
+			const token = localStorage.getItem("token");
+			if (token) {
+				headers.set("Authorization", `Bearer ${token}`);
+			}
+			return headers;
+		},
+	}),
+	endpoints: () => ({}),
 });
 
 export default api;
